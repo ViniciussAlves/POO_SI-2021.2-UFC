b70066eb30cbe31783bc5f087442775a1ed4b1c1
Vinicius Alves <viniciuspvp98@gmail.com>
2021-09-28 23:35:36 -0300

    teste

--- teste.cpp
+++ teste.cpp
@@ -1,5 +1,9 @@
-#include <stdio.h>
-int main() {
-    printf("Hello world");
+#include <iostream>
+using namespace std;
+    auto soma(auto a, auto b) {
+        return a + b;
+    }
+    int main() {
+        std::cout << soma(5, 6) << '\n';
         return 0;
-}
+    }
